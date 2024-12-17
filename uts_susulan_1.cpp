2d188ff6e933b9b066e1da39d4e210c7d92bf91a
Dwiraditya-er <radityaaditya626@gmail.com>
2024-12-17 10:44:40 +0800

    uts_susulan_1.cpp

--- uts_susulan_1.cpp
+++ uts_susulan_1.cpp
@@ -4,11 +4,9 @@ using namespace std;
 int main() {
     int number;
 
-    // Meminta pengguna untuk memasukkan angka
     cout << "Masukkan sebuah angka: ";
     cin >> number;
 
-    // Memeriksa apakah angka genap atau ganjil
     if (number % 2 == 0) {
         cout << number << " adalah Genap." << endl;
     } else {
